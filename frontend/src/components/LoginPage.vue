540a992bf9df5df40a50aa87cc3382783d592781
Javier <ibrahimcruz06@gmail.com>
2026-04-24 18:38:12 -0600

    Form principal para front end de log in

--- frontend/src/components/LoginPage.vue
+++ frontend/src/components/LoginPage.vue
@@ -1,6 +1,38 @@
 <template>
     <div>
-
+        <div class="logo"><h1>Snoopy Airlines</h1></div>
+        <div class="login-form">
+            <h2>Iniciar sesión</h2>
+            <h3>Bienvenido a Snoopy Airlines</h3>
+            <form @submit.prevent="login">
+                <div class="form-group">
+                    <label for="correo">Correo electrónico:</label>
+                    <!--v-model="formData.Email" -->
+                    <input
+                        type="email"
+                        id="email"
+                        class="form-control"
+                        placeholder="nombre@correo.com"
+                        required
+                    />
+                </div>
+                <div class="form-group">
+                    <label for="contrasena">Contraseña:</label>
+                    <!--v-model="formData.Password" -->
+                    <input
+                        type="password"
+                        id="password"
+                        class="form-control"
+                        placeholder="Contraseña"
+                        required
+                    />
+                </div>
+                <button type="submit" class="btn btn-primary">Iniciar sesión</button>
+                <a href="/">
+                    <p>¿Olvidaste tu contraseña?</p>
+                </a>
+            </form>
+        </div>
     </div>
 </template>
 
