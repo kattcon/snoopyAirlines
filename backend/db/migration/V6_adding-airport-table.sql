57d281e6b765a76287fdf0c94c4f68c6a3ac9d91
Kate <kathii.conii@gmail.com>
2026-04-25 12:48:42 -0600

    se realiza migración de tablas necesarias para el user story 4 del sprint 1

--- backend/db/migration/V6_adding-airport-table.sql
+++ backend/db/migration/V6_adding-airport-table.sql
@@ -1,11 +1,7 @@
-CREATE TABLE airport (
-    id INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
-    name VARCHAR(200) NOT NULL,
-    code VARCHAR(3) NOT NULL UNIQUE,
-    city_id INT NOT NULL,
-    timezone VARCHAR(100) NOT NULL,
-    
-    CONSTRAINT fk_airport_city 
-    FOREIGN KEY (city_id) 
-    REFERENCES city(id)
-);
+ALTER TABLE airport ADD name VARCHAR(200) NOT NULL;
+ALTER TABLE airport ADD code VARCHAR(3) NOT NULL;
+ALTER TABLE airport ADD city_id INT NOT NULL;
+ALTER TABLE airport ADD timezone VARCHAR(100) NOT NULL;
+
+ALTER TABLE airport ADD CONSTRAINT uq_airport_code UNIQUE (code);
+ALTER TABLE airport ADD CONSTRAINT fk_airport_city FOREIGN KEY (city_id) REFERENCES city(id);
