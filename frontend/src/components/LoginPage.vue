fa201fc2f28449a1c11c628339d141c7eac8198f
Javier <ibrahimcruz06@gmail.com>
2026-04-24 19:56:06 -0600

    Ajustes de diseno en loginPage, se agrega imagen de Snoopy

--- frontend/src/components/LoginPage.vue
+++ frontend/src/components/LoginPage.vue
@@ -1,12 +1,16 @@
 <template>
-    <div>
-        <div class="logo"><h3>Snoopy Airlines</h3></div>
+    <div class="login-container">
+        <div class="logo">
+            <img src="../assets/logoSA.png" alt="logo" class="logo-image" />
+            <h3>Snoopy Airlines</h3>
+        </div>
         <div class="login-form">
+            <img src="../assets/logoSA.png" alt="logo" class="logo-image-login" />
             <h3 class="login-title">Iniciar sesión</h3>
             <p class="welcome-message">Bienvenido a Snoopy Airlines</p>
             <form @submit.prevent="login">
-                <div class="form-group">
-                    <label for="correo">Correo electrónico:</label>
+                <div class="form-group-email">
+                    <label for="correo" class="email-title">Correo electrónico:</label>
                     <!--v-model="formData.Email" -->
                     <input
                         type="email"
@@ -16,8 +20,8 @@
                         required
                     />
                 </div>
-                <div class="form-group">
-                    <label for="contrasena">Contraseña:</label>
+                <div class="form-group-password">
+                    <label for="contrasena" class="password-title">Contraseña:</label>
                     <!--v-model="formData.Password" -->
                     <input
                         type="password"
@@ -44,21 +48,43 @@
 
 <style>
 
+body {
+    background-color: #f9f9f9;
+}
+
+
 .logo {
     text-align: left;
+    display: flex;
     margin-top: 10px;
+    margin-left: 100px;
     text-size-adjust: 10px;
+
+    .logo-image {
+        align-items: left;
+        width: 50px;
+        height: 50px;
+    }
 }
 
 .login-form {
-    width: 300px;
+    width: 450px;
+    height: 600px;
     margin: 0 auto;
-    padding: 20px;
-    border: 1px solid #ccc;
-    border-radius: 5px;
-    background-color: #f9f9f9;
+    padding: 25px;
+    border-radius: 20px;
+    background-color: #ffffff;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
 
+    .logo-image-login {
+        width: 120px;
+        height: 120px;
+        display: block;
+        margin: 0 auto;
+    }
+    
     .login-title {
+        margin-top: 0 auto;
         text-align: center;
         margin-bottom: 10px;
     }
@@ -77,17 +103,39 @@
         background-color: oklch(0.546 0.245 262.881);
         color: white;
         border: none;
-        border-radius: 5px;
+        border-radius: 15px;
         cursor: pointer;
     }
 
-    .form-group {   
-        margin-bottom: 15px;
+    .form-group-email {   
+        margin-bottom: 20px;
         font-weight: bold;
-        font-size: small;
+        font-size: medium;
+
+        .email-title {
+            margin-bottom: 10px;
+        }
+    }
+
+    .form-group-password {   
+        margin-bottom: 30px;
+        font-weight: bold;
+        font-size: medium;
+
+        .password-title {
+            margin-bottom: 10px;
+        }
+    }
+
+    .form-control {
+        width: 100%;
+        padding: 10px;
+        border: 1px solid #ccc;
+        border-radius: 5px;
     }
 
     .forget-pw {
+        margin-bottom: 30px;
         text-decoration: none;
         text-align: right;
         font-size: small;
