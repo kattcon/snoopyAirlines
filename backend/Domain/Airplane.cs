1bd7cc8eda4db469e5f7d0f907bc4e735a681f44
jordan <jorda@DESKTOP-FN3H86P>
2026-05-01 16:38:55 -0600

    intake and domain definition

---
+++ backend/Domain/Airplane.cs
@@ -0,0 +1,17 @@
+namespace SnoopyAirlines.domain
+{
+    public class Airplane
+    {
+
+        public int modelNumber { get; set; }
+
+        public int touristRows { get; set; }
+        public int touristColumns { get; set; }
+
+        public int firstclassRows { get; set; }
+        public int firstclassColumns { get; set; }
+
+        public float maxWeight { get; set; }
+    }
+
+}
---
+++ backend/Domain/Intake/AirplaneIntake.cs
@@ -0,0 +1,12 @@
+﻿namespace SnoopyAirlines.Domain.Intake
+{
+    public class AirplaneIntake
+    {
+        required public int ModelNumber { get; set; }
+        required public int TouristRows { get; set; }
+        required public int TouristColumns { get; set; }
+        required public int FirstclassRows { get; set; }
+        required public int FirstclassColumns { get; set; }
+        required public float MaxWeight { get; set; }
+    }
+}
