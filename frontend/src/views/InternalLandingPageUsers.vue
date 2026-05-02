ee2a76fb5dcbd25f6e4c6b7f70e9903371a40a9a
Javier <ibrahimcruz06@gmail.com>
2026-05-01 20:35:47 -0600

    Creacion de view de administrar usuarios

--- frontend/src/views/InternalLandingPageUsers.vue
+++ frontend/src/views/InternalLandingPageUsers.vue
@@ -1,8 +1,16 @@
 <template>
-    <div>
-        <h1 class="vuelos-text">Administrar Usuarios</h1>
-        <button>Usuarios</button>
+    <div class="subsection-header">
+        <h3 class="subsection-header-title">Administrar Usuarios</h3>
+        <h6 class="subsection-header-subtitle">Gestiona y registra nuevos usuarios del sistema</h6>
     </div>
+    <div class="container">
+        <div class="section-div">
+            <h4 class="main-text-section">Registrar Usuario</h4>
+            <p class="subtext-aeronave">Agrega un nuevo usuario al sistema</p>
+            <router-link to="/createFlight" class="router-button">Registrar Usuario</router-link> <!--CAMBIAR POR LA RUTA DE Registrar usuario-->
+        </div>
+    </div>
+
 </template>
 
 <script>
@@ -12,11 +20,6 @@
 </script>
 
 <style>
-
-body {
-    justify-content: center;
-    display: flex;
-}
 
 
 </style>
