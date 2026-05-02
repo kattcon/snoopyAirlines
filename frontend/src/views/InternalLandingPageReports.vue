da3b79dd58b64f0b5f6c56720db947e213cb1e13
Javier <ibrahimcruz06@gmail.com>
2026-05-01 20:38:32 -0600

    Creacion de view para reportes del sistema

--- frontend/src/views/InternalLandingPageReports.vue
+++ frontend/src/views/InternalLandingPageReports.vue
@@ -1,8 +1,16 @@
 <template>
-    <div>
-        <h1 class="vuelos-text">Reportes</h1>
-        <button>Reportes</button>
+    <div class="subsection-header">
+        <h3 class="subsection-header-title">Reportes</h3>
+        <h6 class="subsection-header-subtitle">Visualizar reportes del sistema</h6>
     </div>
+    <div class="container">
+        <div class="section-div">
+            <h4 class="main-text-section">Sección en desarrollo</h4>
+            <p class="subtext-aeronave">Los reportes estarán disponibles próximamente</p>
+            <router-link to="/" class="router-button">Volver a home</router-link>
+        </div>
+    </div>
+
 </template>
 
 <script>
@@ -12,11 +20,6 @@
 </script>
 
 <style>
-
-body {
-    justify-content: center;
-    display: flex;
-}
 
 
 </style>
