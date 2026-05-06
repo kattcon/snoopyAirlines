3f381feb061a524aa21690767a1d8a7ee7e668ae
jordan <jorda@DESKTOP-FN3H86P>
2026-05-05 22:12:26 -0600

    show more aircraft data

--- frontend/src/views/AircraftsList.vue
+++ frontend/src/views/AircraftsList.vue
@@ -52,8 +52,12 @@ export default{
             searchTerm:"",
             errorMsg:"",
             aircraftColumns:[
-                {key: "model", label:"Modelo"}
-                //  agregar mas columnas
+                {key: "model", label:"Modelo"},
+                {key: "touristRows", label:"Filas clase turista"},
+                {key: "touristColumns", label:"Asientos por fila clase turista"},
+                {key: "firstclassRows", label:"Filas clase ejecutiva"},
+                {key: "firstclassColumns", label:"Asientos por fila clase ejecutiva"},
+                {key: "maxWeight", label:"Peso máximo permitido"}
             ]
         }
     },
