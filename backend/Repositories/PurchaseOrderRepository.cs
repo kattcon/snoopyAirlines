3b9c91cc79d4484771db6f9200c1a9adc51f4765
Javier <ibrahimcruz06@gmail.com>
2026-05-31 19:16:06 -0600

    Added GetPurchaseOrderDetails calling in purchaseOrder Repository

--- backend/Repositories/PurchaseOrderRepository.cs
+++ backend/Repositories/PurchaseOrderRepository.cs
@@ -62,5 +62,52 @@ namespace snoopy_airlines_backend.Repositories
                 """);
             return order.ToList().AsReadOnly();
         }
+
+        public async Task<PurchaseOrderEmailData?> GetPurchaseOrderDetailsAsync(
+            int purchaseOrderId,
+            CancellationToken cancellationToken)
+        {
+            using var connection = new SqlConnection(_connectionString);
+            await connection.OpenAsync(cancellationToken);
+
+            var rows = await connection.QueryAsync<PurchaseOrderDetailRow>("""
+                SELECT * FROM GetPurchaseOrderDetails(@PurchaseOrderId)
+                """, new { PurchaseOrderId = purchaseOrderId });
+            
+            var list = rows.ToList();
+
+            if (list.Count == 0)
+            {
+                return null;
+            }
+
+            var first = list[0];
+
+            return new PurchaseOrderEmailData
+            {
+                PurchaseOrderId  = first.PurchaseOrderId,
+                SeatClass = first.SeatClass,
+                DepartureTime = first.DepartureTime,
+                ArrivalTime = first.ArrivalTime,
+                DepartureAirportName = first.DepartureAirportName,
+                DepartureAirportCode = first.DepartureAirportCode,
+                DepartureCityName = first.DepartureCityName,
+                ArrivalAirportName = first.ArrivalAirportName,
+                ArrivalAirportCode = first.ArrivalAirportCode,
+                ArrivalCityName = first.ArrivalCityName,
+                AirplaneModel = first.AirplaneModel,
+                Passengers = list.Select(r => new PassengerEmailData
+                {
+                    FirstName = r.FirstName,
+                    LastName = r.LastName,
+                    Gender = r.Gender,
+                    Nationality = r.Nationality,
+                    BirthDay = r.BirthDay,
+                    BirthMonth = r.BirthMonth,
+                    BirthYear = r.BirthYear
+                }).ToList(),
+            };
+        }
+
     }
 }
