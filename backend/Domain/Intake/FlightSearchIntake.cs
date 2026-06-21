dec4d22baf5858ff3fac2ff0ccfbfd27a923e0df
Javier <ibrahimcruz06@gmail.com>
2026-06-20 21:30:01 -0600

    Removed FlightSearchIntake as it's not being used

--- backend/Domain/Intake/FlightSearchIntake.cs
+++
@@ -1,8 +0,0 @@
-namespace SnoopyAirlines.Domain.Intake
-{
-    public class FlightSearchIntake
-    {
-        public required string ConfirmationNumber { get; set; }
-        public required string LastNames { get; set; }
-    }
-}
