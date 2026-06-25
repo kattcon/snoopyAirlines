d957864e80e6e91a8b0a7cf2e9e7cedb0d16dd9f
Javier <ibrahimcruz06@gmail.com>
2026-06-24 20:14:04 -0600

    Ajuste en FlightSearchRepository para incluir pasajeros

--- backend/Repositories/FlightSearchRepository.cs
+++ backend/Repositories/FlightSearchRepository.cs
@@ -22,17 +22,44 @@ namespace SnoopyAirlines.Repositories
             using var connection = new SqlConnection(_connectionString);
             await connection.OpenAsync(cancellationToken);
 
-            var CommandDefinition = new CommandDefinition(
-                """
-                SELECT * FROM dbo.GetFlightReportByConfirmation(@ConfirmationNumber, @LastNames)
-                ORDER BY SequenceNumber;
-                """,
-                new { ConfirmationNumber = confirmationNumber, LastNames = lastNames },
-                cancellationToken: cancellationToken);
+            var legs = await connection.QueryAsync<FlightReportView>(
+                new CommandDefinition(
+                    """
+                    SELECT * FROM dbo.GetFlightReportByConfirmation(@ConfirmationNumber, @LastNames)
+                    ORDER BY SequenceNumber;
+                    """,
+                        new { ConfirmationNumber = confirmationNumber, LastNames = lastNames },
+                        cancellationToken: cancellationToken));
+            
+            var legsList = legs.ToList();
 
-            var flightReport = await connection.QueryAsync<FlightReportView>(CommandDefinition);
-
-            return flightReport.ToList();
+            var passengers = await connection.QueryAsync<PassengerReportView>(
+                new CommandDefinition(
+                    """
+                    SELECT
+                        p.FirstName,
+                        p.LastName,
+                        p.Gender,
+                        p.Nationality,
+                        p.Birthday,
+                        p.BirthMonth,
+                        p.BirthYear,
+                        p.CarryOnLuggage,
+                        p.CheckedLuggage
+                    FROM dbo.Passenger p
+                    JOIN dbo.booking b ON p.PurchaseOrderId = b.purchase_order_id
+                    WHERE b.confirmation_code = UPPER(LTRIM(RTRIM(@ConfirmationNumber)))
+                    ORDER BY p.Id;
+                    """,
+                    new { ConfirmationNumber = confirmationNumber },
+                    cancellationToken: cancellationToken));
+            
+            return new FlightSearchResult
+            {
+                Legs = legsList,
+                Passengers = passengers.ToList()
+            };
+            
         }
     }
 }
