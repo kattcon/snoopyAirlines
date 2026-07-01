ea01cfe87c7d492ec81acddef1ac7f2c312234be
JorBrenesJ <jorbrenesj@gmail.com>
2026-07-01 02:48:46 -0600

    fix pow

--- frontend/src/components/LuggageModifyPopUp.vue
+++ frontend/src/components/LuggageModifyPopUp.vue
@@ -269,8 +269,9 @@ export default {
         }
     },
     checkedLuggageCostForPassenger(bags, unitPrice, multiplier) {
-        if (bags <= 0) return 0;
-        return unitPrice * Math.pow((1 + multiplier), bags);
+        if (bags < 0) return 0;
+        if (bags === 0) bags = 1; 
+        return unitPrice * Math.pow((1 + multiplier), bags - 1);
     },
     totalCostForPassenger(passenger){
         return this.routes.reduce((sum, route) => {
