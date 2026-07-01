9da6ac8b7cd5874be709e570b42dae1c71e25780
Jeshua Reyes <jeshuakrc@gmail.com>
2026-06-30 19:31:25 -0600

    Adding indexes for query optimization

---
+++ backend/db/migration/V65_query-performance-indexes.sql
@@ -0,0 +1,15 @@
+CREATE INDEX IX_route_departure_lookup
+ON dbo.[route] (departure_airport_id, is_deleted, departure_time);
+GO
+
+CREATE INDEX IX_route_arrival_lookup
+ON dbo.[route] (arrival_airport_id, is_deleted, arrival_time, departure_time);
+GO
+
+CREATE INDEX IX_route_airplane_lookup
+ON dbo.[route] (airplane_id, is_deleted);
+GO
+
+CREATE INDEX IX_flight_departure_at
+ON dbo.flight (departure_at);
+GO
